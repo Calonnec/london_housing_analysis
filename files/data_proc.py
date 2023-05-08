8c084412028b5b26f6316e05044d90f4063ceccf
thierry calonnec <thierry.calonnec@yahoo.fr>
2023-05-08 10:59:44 +0100

    main work done

--- files/data_proc.py
+++ files/data_proc.py
@@ -11,27 +11,38 @@ def data_read(path):
     data_buyer_raw = pd.read_csv(path[0]).dropna()
     data_prop = pd.read_csv(path[1]).dropna()
     data_prop_type = pd.read_csv(path[2]).dropna()
+    data_eng = pd.read_csv(path[3])
 
     data_buyer_raw["Period"] = pd.to_datetime(data_buyer_raw["Period"], yearfirst=True)
     data_prop["Period"] = pd.to_datetime(data_prop["Period"], yearfirst=True)
     data_prop_type["Period"] = pd.to_datetime(data_prop_type["Period"], yearfirst=True)
+    data_eng["Period"] = pd.to_datetime(data_eng["Period"], yearfirst=True)
 
-    return data_buyer_raw, data_prop, data_prop_type
+    return data_buyer_raw, data_prop, data_prop_type, data_eng
 
 def make_data_yearly(data):
     data_yearly = data.groupby(by=pd.Grouper(key="Period", freq='Y')).mean().reset_index()
     data_yearly["Period"] = data_yearly["Period"].apply(lambda x: x.year)
     return data_yearly
 
-def make_revenue_data():
+def make_revenue_data(area):
     path = "data/Revenue/"
     csv_file = glob.glob(os.path.join(path, "*Total, weekly.csv"))
     data_test = pd.read_csv(csv_file[0])
-    data = data_test[data_test["Area"] == "London"].drop(["Code", "Area"], axis=1).T[::2].reset_index().rename(columns={42:"salary","index":"year"})
+    data = data_test[data_test["Area"] == area].drop(["Code", "Area"], axis=1).T[::2].reset_index()
+    data = data.rename(columns={data.columns[-1]:"salary","index":"year"})
     data = data.apply(lambda col:pd.to_numeric(col, errors='coerce'))
     return data
 
-def make_sal_price_graph(data_prop, data_sal):
+def make_data_sal_in_fold(data, data_sal):
+    data["Average price All property types in salary fold"] = data["Average price All property types"] / (data_sal["salary"] *52)
+    data["Average price Detached houses in salary fold"] = data["Average price Detached houses"] / (data_sal["salary"] *52)
+    data["Average price Semi-detached houses in salary fold"] = data["Average price Semi-detached houses"]/ (data_sal["salary"] *52)
+    data["Average price Terraced houses in salary fold"] = data["Average price Terraced houses"] / (data_sal["salary"] *52)
+    data["Average price Flats and maisonettes in salary fold"] = data["Average price Flats and maisonettes"] / (data_sal["salary"] *52)
+    return data
+
+def make_sal_price_graph(data_prop, data_sal, h=600, w=1000):
     y_all=data_prop["Average price All property types"]/data_prop["Average price All property types"][0]
     y_det=data_prop["Average price Detached houses"]/data_prop["Average price Detached houses"][0]
     y_semi=data_prop["Average price Semi-detached houses"]/data_prop["Average price Semi-detached houses"][0]
@@ -48,28 +59,24 @@ def make_sal_price_graph(data_prop, data_sal):
     fig.add_trace(go.Scatter(name='Price Flat and Maisonette', x=data_prop["Period"], y=y_flat))
     fig.add_trace(go.Scatter(name="Average Weekly salary", x=data_sal["year"], y=y_salary))
 
-    fig.update_layout(height=600, width=1000, title="House and yearly salary increase in fold over the year")
+    fig.update_layout(height=h, width=w, title="House and yearly salary increase in fold over the year")
     fig.update_xaxes(title_text="year")
     fig.update_yaxes(title_text="Average increase in fold")
 
     return fig
 
-def make_price_fold_graph(data_prop, data_sal):
+def make_price_fold_graph(data_prop, h=600, w=1000):
 
-    fig = make_subplots(rows=2, cols=1)
-    fig.add_trace(go.Scatter(name='All Properties', x=data_prop["Period"], y=data_prop["Average price All property types in salary fold"], legendgroup="1"), row=1, col=1)
-    fig.add_trace(go.Scatter(name='Detached Houses', x=data_prop["Period"], y=data_prop["Average price Detached houses in salary fold"], legendgroup="1"), row=1, col=1)
-    fig.add_trace(go.Scatter(name='Semi Detached Houses', x=data_prop["Period"], y=data_prop["Average price Semi-detached houses in salary fold"], legendgroup="1"), row=1, col=1)
-    fig.add_trace(go.Scatter(name='Terraced Houses', x=data_prop["Period"], y=data_prop["Average price Terraced houses in salary fold"], legendgroup="1"), row=1, col=1)
-    fig.add_trace(go.Scatter(name='Flat and Maisonette', x=data_prop["Period"], y=data_prop["Average price Flats and maisonettes in salary fold"], legendgroup="1"), row=1, col=1)
+    fig = make_subplots()
+    fig.add_trace(go.Scatter(name='All Properties', x=data_prop["Period"], y=data_prop["Average price All property types in salary fold"]))
+    fig.add_trace(go.Scatter(name='Detached Houses', x=data_prop["Period"], y=data_prop["Average price Detached houses in salary fold"]))
+    fig.add_trace(go.Scatter(name='Semi Detached Houses', x=data_prop["Period"], y=data_prop["Average price Semi-detached houses in salary fold"]))
+    fig.add_trace(go.Scatter(name='Terraced Houses', x=data_prop["Period"], y=data_prop["Average price Terraced houses in salary fold"]))
+    fig.add_trace(go.Scatter(name='Flat and Maisonette', x=data_prop["Period"], y=data_prop["Average price Flats and maisonettes in salary fold"]))
 
-    fig.add_trace(go.Scatter(name="Average Yearly salary", x=data_sal["year"], y=data_sal["salary"]*52, legendgroup="2"), row = 2, col=1)
-
-    fig.update_layout(height=800, width=1000, title="House price expressed in fold of the yearly salary",legend_tracegroupgap = 280)
-    fig.update_xaxes(title_text="year", row=1, col=1)
-    fig.update_yaxes(title_text="Average increase in fold", row=1, col=1)
-    fig.update_xaxes(title_text="year", row=2, col=1)
-    fig.update_yaxes(title_text="Average Yearly Salary (£)", row=2, col=1)
+    fig.update_layout(height=h, width=w, title = "House prices per type expressed in fold of Yearly salary")
+    fig.update_xaxes(title_text="year")
+    fig.update_yaxes(title_text="Average increase in fold")
 
     return fig
 
@@ -117,37 +124,38 @@ def make_price_borough_graph(data_prop, borough):
     fig.update_xaxes(title_text="year")
     fig.update_yaxes(title_text="Average property prices")
 
-    fig.update_layout(height=600, width=800, title=f'Property prices over the years in {borough}')
+    fig.update_layout(height=400, width=800, title=f'Property prices over the years in {borough}')
 
     return fig
 
 def make_price_sal_fold_borough_data(data_prop, data_sal, borough):
-    b_data_prop = data_prop[borough].groupby(by=pd.Grouper(key="Period", freq='Y')).mean().reset_index()
-    b_data_prop["Period"] = b_data_prop["Period"].apply(lambda x: x.year)
-    b_data_prop_trim = b_data_prop[b_data_prop["Period"]>=2002].reset_index()
+    if data_sal.get("salary") is not None:
+        b_data_prop = data_prop[borough].groupby(by=pd.Grouper(key="Period", freq='Y')).mean().reset_index()
+        b_data_prop["Period"] = b_data_prop["Period"].apply(lambda x: x.year)
+        b_data_prop_trim = b_data_prop[b_data_prop["Period"]>=2002].reset_index()
 
-    y_prop=b_data_prop_trim["Average price All property types"]/b_data_prop_trim["Average price All property types"][0]
-    y_det=b_data_prop_trim["Average price Detached houses"]/b_data_prop_trim["Average price Detached houses"][0]
-    y_semi=b_data_prop_trim["Average price Semi-detached houses"]/b_data_prop_trim["Average price Semi-detached houses"][0]
-    y_ter=b_data_prop_trim["Average price Terraced houses"]/b_data_prop_trim["Average price Terraced houses"][0]
-    y_flat=b_data_prop_trim["Average price Flats and maisonettes"]/b_data_prop_trim["Average price Flats and maisonettes"][0]
-    y_salary = data_sal["salary"]/data_sal["salary"][0]
+        y_prop=b_data_prop_trim["Average price All property types"]/b_data_prop_trim["Average price All property types"][0]
+        y_det=b_data_prop_trim["Average price Detached houses"]/b_data_prop_trim["Average price Detached houses"][0]
+        y_semi=b_data_prop_trim["Average price Semi-detached houses"]/b_data_prop_trim["Average price Semi-detached houses"][0]
+        y_ter=b_data_prop_trim["Average price Terraced houses"]/b_data_prop_trim["Average price Terraced houses"][0]
+        y_flat=b_data_prop_trim["Average price Flats and maisonettes"]/b_data_prop_trim["Average price Flats and maisonettes"][0]
+        y_salary = data_sal["salary"]/data_sal["salary"][0]
 
-    fig = make_subplots()
+        fig = make_subplots()
 
-    fig.add_trace(go.Scatter(name='Average price All property types', x=b_data_prop_trim["Period"], y=y_prop))
-    fig.add_trace(go.Scatter(name='Average price Detached House', x=b_data_prop_trim["Period"], y=y_det))
-    fig.add_trace(go.Scatter(name='Average price Semi Detached House', x=b_data_prop_trim["Period"], y=y_semi))
-    fig.add_trace(go.Scatter(name='Average price Terraced House', x=b_data_prop_trim["Period"], y=y_ter))
-    fig.add_trace(go.Scatter(name='Average price Flat and Maisonette', x=b_data_prop_trim["Period"], y=y_flat))
-    fig.add_trace(go.Scatter(name="Average Weekly salary", x=data_sal["year"], y=y_salary))
+        fig.add_trace(go.Scatter(name='Average price All property types', x=b_data_prop_trim["Period"], y=y_prop))
+        fig.add_trace(go.Scatter(name='Average price Detached House', x=b_data_prop_trim["Period"], y=y_det))
+        fig.add_trace(go.Scatter(name='Average price Semi Detached House', x=b_data_prop_trim["Period"], y=y_semi))
+        fig.add_trace(go.Scatter(name='Average price Terraced House', x=b_data_prop_trim["Period"], y=y_ter))
+        fig.add_trace(go.Scatter(name='Average price Flat and Maisonette', x=b_data_prop_trim["Period"], y=y_flat))
+        fig.add_trace(go.Scatter(name="Average Weekly salary", x=data_sal["year"], y=y_salary))
 
-    fig.update_xaxes(title_text="year")
-    fig.update_yaxes(title_text="Increase in fold")
+        fig.update_xaxes(title_text="year")
+        fig.update_yaxes(title_text="Increase in fold")
 
-    fig.update_layout(height=600, width=800, title=f'House and salary increase in fold over the year in {borough}')
-
-    return fig
+        fig.update_layout(height=400, width=800, title=f'House and salary increase in fold over the year in {borough}')
+        return fig
+    else: return "Data unavailable"
 
 def make_data_for_map(data_dict, value_to_see):
     res = pd.DataFrame()
@@ -192,10 +200,10 @@ def make_map(raw_data, year):
                                 color_continuous_scale='Viridis',
                                 mapbox_style='carto-positron',
                                 center={'lat': 51.5074, 'lon': -0.1278},
-                                zoom=9.3,
+                                zoom=9,
                                 opacity=0.5,
                                 hover_name='name',
                                 labels={'value': 'Data Value'}
                             )
-    fig.update_layout(width=1200, height = 800)
+    fig.update_layout(width=800, height = 600)
     return fig
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,28 @@
 import streamlit as st
 import plotly.express as px
 from files.data_proc import data_read, make_data_yearly, make_sal_price_graph, make_revenue_data, make_price_fold_graph, make_borough_data
-from files.data_proc import make_borough_sal_data, make_price_borough_graph, make_price_sal_fold_borough_data, make_data_for_map, make_map
+from files.data_proc import make_borough_sal_data, make_price_borough_graph, make_price_sal_fold_borough_data, make_data_for_map, make_map, make_data_sal_in_fold
 
 
 data_path = ["data/ukhpi-buyer-status-london-from-1980-01-01-to-2023-01-01.csv",
              "data/ukhpi-property-status-london-from-1980-01-01-to-2023-01-01.csv",
-             "data/ukhpi-property-type-london-from-1980-01-01-to-2023-01-01.csv"
+             "data/ukhpi-property-type-london-from-1980-01-01-to-2023-01-01.csv",
+             "data/ukhpi-england-from-1996-01-01-to-2023-01-01.csv"
              ]
-
-data_buyer, data_prop, data_prop_type = data_read(data_path)
+data_buyer, data_prop, data_prop_type, data_eng = data_read(data_path)
 
 # Data maker ---------------------------------------------------------------------------------
 data_prop_yearly = make_data_yearly(data_prop)
 data_buyer_yearly = make_data_yearly(data_buyer)
 data_prop_type_yearly = make_data_yearly(data_prop_type)
-total_weekly = make_revenue_data()
+total_weekly = make_revenue_data("London")
 data_prop_type_yearly_trim = data_prop_type_yearly[data_prop_type_yearly["Period"] >= 2002].reset_index()
 borough_data = make_borough_data()
+data_eng = make_data_yearly(data_eng)
+data_eng_trim = data_eng[data_eng["Period"] >= 2002].reset_index()
 
-data_prop_type_yearly_trim["Average price All property types in salary fold"] = data_prop_type_yearly_trim["Average price All property types"] / (total_weekly["salary"] *52)
-data_prop_type_yearly_trim["Average price Detached houses in salary fold"] = data_prop_type_yearly_trim["Average price Detached houses"] / (total_weekly["salary"] *52)
-data_prop_type_yearly_trim["Average price Semi-detached houses in salary fold"] = data_prop_type_yearly_trim["Average price Semi-detached houses"]/ (total_weekly["salary"] *52)
-data_prop_type_yearly_trim["Average price Terraced houses in salary fold"] = data_prop_type_yearly_trim["Average price Terraced houses"] / (total_weekly["salary"] *52)
-data_prop_type_yearly_trim["Average price Flats and maisonettes in salary fold"] = data_prop_type_yearly_trim["Average price Flats and maisonettes"] / (total_weekly["salary"] *52)
-
-
+data_prop_type_yearly_trim = make_data_sal_in_fold(data_prop_type_yearly_trim, total_weekly)
+data_eng_trim = make_data_sal_in_fold(data_eng_trim, make_revenue_data("England and Wales"))
 
 # Fig maker------------------------------------------------------------------------------------
 prop_type_list = ["Average price All property types","Average price Detached houses","Average price Semi-detached houses","Average price Terraced houses","Average price Flats and maisonettes"]
@@ -49,13 +46,25 @@ fig_first_vs_former_rate = px.bar(data_buyer_yearly, x="Period", y=["Percentage 
 fig_prop_type = px.line(data_prop_type_yearly, x="Period", y=prop_type_list, title="Average price in London of properties per year (£)",
                           labels={"value": "average price (£)", "Period": "Year", "variable": "Legend"}, width=800)
 
+fig_prop_type_eng = px.line(data_eng, x="Period", y=prop_type_list, title="Average price in England and Wales of properties per year (£)",
+                          labels={"value": "average price (£)", "Period": "Year", "variable": "Legend"}, width=800)
+
 fig_trend_prop_price = px.scatter(data_prop_type_yearly, x="Period", y=prop_type_list ,trendline="ols",
-                title="Average price per property types trends",
+                title=" Trends for average price per property types",
                 labels={"value": "average price (£)", "Period": "Year", "variable": "Legend"},
                 width=800)
 
 fig_sales_volume = px.line(data_prop_type_yearly, x="Period", y="Sales volume", title="Number of sales of all property in London per year",
                            labels={"value": "average price (£)", "Period": "Year", "variable": "Legend"},height=400, width=600)
+
+fig_sales_volume_eng = px.line(data_eng, x="Period", y="Sales volume", title="Number of sales of all property in England and Wales per year",
+                           labels={"value": "average price (£)", "Period": "Year", "variable": "Legend"},height=400, width=600)
+
+fig_sal_london = px.line(total_weekly, x="year", y=total_weekly["salary"]*52, title = "Average Yearly salary in London", height=400, width=600)
+fig_sal_london.update_yaxes(title_text = "Average salary (£)")
+
+fig_sal_eng = px.line(make_revenue_data("England and Wales"), x="year", y=make_revenue_data("England and Wales")["salary"]*52, title = "Average Yearly salary in England and Wales", height=400, width=600)
+fig_sal_eng.update_yaxes(title_text = "Average salary (£)")
 
 # Session state --------------------------------------------------------------------------------------------
 
@@ -88,66 +97,97 @@ def click_2():
 
 # App layout ----------------------------------------------------------------------------------------------
 
-st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+#st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
-colh, colb = st.columns([0.8, 0.2])
+st.header("London housing prices visualisation")
+colh, colb = st.columns([0.7, 0.3])
 
-colh.header("London housing prices visualisation")
 colb.button(st.session_state["but_label"], key="button1", on_click=click)
 
-st.markdown("Text here explaining where data come from and aim")
+st.markdown("""The aim of this dashboard is to visualise the evolution of housing prices in London (and its borough) and compare them to revenue data of its inhabitant.
+            Also I want to compare that data to the whole of the UK in order to see how inflated are house prices in London.
+            Data for the housing prices have been sourced from the Land Registry: UK house Price Index. https://landregistry.data.gov.uk/app/ukhpi/browse
+            Data for the revenue have been sourced from the Organisation of National Statistics.""")
 
-# London Data -------------------------------------------------------------------------------
+#England and Wales Data ----------------------------------------------------------------------------
 
 if st.session_state["but_state"] == False:
+
+    st.markdown("### For reference, let's look at the England and Wales data.")
+
+    #col_e1, col_e2 = st.columns(2)
+
+    st.plotly_chart(fig_prop_type_eng)
+    st.plotly_chart(fig_sal_eng)
+
+    st.markdown("""The graphs below are comparing the house prices with the average yearly salary.
+                Top, the housing prices are expressed in fold of average price (How much of the yearly salary a house cost).
+                Below, the increase of prices and salary expressed in fold of their first value taken in 2002. It is meant to show if the salary increase are keeping up with house prices.""")
+
+    #col_e3, col_e4 = st.columns(2)
+
+    st.plotly_chart(make_price_fold_graph(data_eng_trim, 400, 800))
+    st.plotly_chart(make_sal_price_graph(data_eng_trim, make_revenue_data("England and Wales"), 400, 800))
+
+    st.markdown("""My original thought was that an expensive house was calculated to be more than 5 to 7 times the average yearly salary. And this was the case in 2002 where average price of all properties in England and Wales were at 6.5 times average yearly salaries.
+                But this hasn't been the case in the last 2 decades where it has been hovering between 8 to 10 times the average yearly salary.
+                The graphs also show that the house prices has been climbing much faster than salaries, now being 2 to 3 times more than in 2002, compare to only 54% increase for salaries.""")
+
+    # London Data -------------------------------------------------------------------------------
+
     #New Build VS Existing Build ------------------------------------------------------------
 
     st.markdown("### Let's compare new build and existing build prices")
 
-    col1, col2 = st.columns(2)
+    #col1, col2 = st.columns(2)
 
-    col1.plotly_chart(fig_new_vs_exist)
-    col2.plotly_chart(fig_new_vs_exist_rate)
+    st.plotly_chart(fig_new_vs_exist)
+    st.plotly_chart(fig_new_vs_exist_rate)
 
-    st.markdown("Text here explaining that new build are not cheaper")
+    st.markdown("""As we can see on the graph above, new builds in London are not cheaper that existing build. They can even been seen to be slightly more expensive recently.
+                So the only way new build can help with the rise of prices is to use them to increase the supply of available housing.""")
 
     #First time buyer vs Exisiting buyer ---------------------------------------------------------
 
     st.markdown("### Let's compare first time buyer and former-owner buyer budgets")
 
-    col3, col4 = st.columns(2)
+    #col3, col4 = st.columns(2)
 
-    col3.plotly_chart(fig_first_vs_former)
-    col4.plotly_chart(fig_first_vs_former_rate)
+    st.plotly_chart(fig_first_vs_former)
+    st.plotly_chart(fig_first_vs_former_rate)
 
-    st.markdown("Text here explaining that first time buyer are poorer as expected")
+    st.markdown("""As expected, first time buyer have a lower budget than former owners and consisitantly buy cheaper housing.
+                It is worth noting that first time buyer are paying more than £400000 in average since 2016 on their first home which is a substantial amount of money to find without having a property to sell.""")
 
     #Property type prices
 
     st.markdown("### Let's look at property prices according to their types")
 
-    col5, col6 = st.columns(2)
+    #col5, col6 = st.columns(2)
 
-    col5.plotly_chart(fig_prop_type)
-    col5.plotly_chart(fig_sales_volume)
-    col6.plotly_chart(fig_trend_prop_price)
-    col6.markdown("Mortgage data here??")
+    st.plotly_chart(fig_prop_type)
+    st.plotly_chart(fig_trend_prop_price)
+    st.plotly_chart(fig_sales_volume)
+    st.plotly_chart(fig_sal_london)
 
-    st.markdown("""Text here explaining that:
-                - 2009 crash is visible
-                - Price keep increasing despite teh crash
-                - The more private the house, the more its price grew""")
+    st.markdown("""While it was visible on the other graphs, we can see the influence the 2008 fincial crash had on the market: the number of houses buying sold halfed in 2009 and nerver went back to its pre crash peak.
+                With the exception of 2009, the housing prices kept increasing despite the crash.
+                While the prices for all type of properties increases, the more private is the property, the more valuable it is.
+                So we can see that since 1996, the pirces of Terraced houses increased by nearly £33000 per years compare to flats where their prices increased £15000 per year.""")
 
     st.markdown("### Let's compare with London's salaries")
     st.markdown("Text about that before 2000 in the rest of country a normal house cost around 4 to 5 time yearly salaries (according to the Bank Of england data).")
 
     st.markdown("The following graph shows the increase of prices and salaries in comparisation of their first value. So for example, in 2015, the price of Terraced House were 2.41 times more expensive than in 2002 (reference).")
 
-    st.plotly_chart(make_sal_price_graph(data_prop_type_yearly_trim, total_weekly))
-    st.markdown("Text explaining that house prices are rising much faster than salaries")
+    st.plotly_chart(make_sal_price_graph(data_prop_type_yearly_trim, total_weekly, 400, 800))
+    st.markdown("""We can see on this graph that housing prices are increasing much faster than income in London.
+                Since 2002, salaries icreased by around 46% in 2021. While property prices increase by 150% to 200% since 2002.
+                We can also note that the prices of flats in London didn't follow the same trend of prices than the other during the COVID 19 pandemic.""")
 
-    st.plotly_chart(make_price_fold_graph(data_prop_type_yearly_trim, total_weekly))
-    st.markdown("Text showing that house price in London are far more than 5 to 7 times yearly salaries.")
+    st.plotly_chart(make_price_fold_graph(data_prop_type_yearly_trim, 400, 800))
+    st.markdown("""We can see that houses price in London are much greater than the rest of country which is around 5 to 7 times average yearly salary.
+                Here even flats are around 13 times the average salary in 2022 while the price of Terraced houses are a stagering 31 time the average salary.""")
 
 # Borough Data ----------------------------------------------------------------------------------
 
@@ -160,11 +200,14 @@ if st.session_state["but_state"] == True:
     if st.session_state["but_bo_state"] == False:
         borough = col_dr.selectbox("Borough",borough_data.keys() ,key="dropdown_1")
 
-
         borough_sal = make_borough_sal_data(borough)
-        col_b1, col_b2 = st.columns(2)
-        col_b1.plotly_chart(make_price_borough_graph(borough_data,borough))
-        col_b2.plotly_chart(make_price_sal_fold_borough_data(borough_data, borough_sal, borough))
+        #col_b1, col_b2 = st.columns(2)
+        st.plotly_chart(make_price_borough_graph(borough_data,borough))
+
+        data_spec_bo = make_price_sal_fold_borough_data(borough_data, borough_sal, borough)
+        if data_spec_bo != "Data unavailable":
+            st.plotly_chart(data_spec_bo)
+        else: st.markdown("Salary data unavailable")
 
     else:
         value_list = prop_type_list
